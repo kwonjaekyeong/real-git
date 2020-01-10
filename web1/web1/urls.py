1aac44d227234c95d4ecd0f75e16fe2d4c850080
kwonjaekyeong <worud38@naver.com>
2020-01-10 16:40:11 +0900

    1.10

--- web1/web1/urls.py
+++ web1/web1/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('member/', include('member.urls')),
     path('board/', include('board.urls')),
+    path('api/', include('api.urls')),
     
 ]
